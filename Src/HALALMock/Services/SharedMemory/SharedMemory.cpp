cce5693b0bc8e545d03a02ed5a35d6d60ef33ff3
oganigl <osganig@gmail.com>
2024-10-25 16:46:02 +0200

    add start() function to shared memory creates the shared memory, assigns the gpio_size and makes gpio_memory to point to the beggining of the shared memory.

--- Src/HALALMock/Services/SharedMemory/SharedMemory.cpp
+++ Src/HALALMock/Services/SharedMemory/SharedMemory.cpp
@@ -1,8 +1,34 @@
 
 #include "HALALMock/Services/SharedMemory/SharedMemory.hpp"
-
+//includes to create the shared Memory in Posix
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
 void SharedMemory::start() {
-    // TODO: create / open shared memory
+    //Create GPIO_Memory
+	const char* 
+	int shm_gpio_fd;
+	//create shared memory object
+	shm_gpio_fd = shm_open(gpio_memory_name, O_CREAT | O_RDWR,0666);
+	if(shm_gpio_fd == -1){
+		ErrorHandler("%s","Error to Open de Shared Memory");
+		return;
+	}
+	//configure the size of the shared memory object
+	if(ftruncate(shm_gpio_fd,gpio_memory_size) == -1){
+		ErrorHandler("%s","Error to asssign memory to the Shared Memory");
+		close(shm_gpio_fd);
+		return;
+	}
+	//point gpio_memory to the beginning of shared_memory
+	gpio_memory = mmap(0, gpio_memory_size, PROT_WRITE | PROT_READ, MAP_SHARED, shm_gpio_fd, 0);
+	if(gpio_memory == MAP_FAILED){
+		ErrorHandler("%s", "Error al mapear la memoria compartida");
+        close(shm_gpio_fd);  // Close the descriptor if there is a problem with the mapping
+        return;
+	}
 }
 EmulatedPin &SharedMemory::get_pin(Pin pin){
     uint8_t offset;
