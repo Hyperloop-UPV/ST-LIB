ce166b870b57d83d71ae1e2c5f7a718000d0605c
jdmarmen <jdmarmen@etsinf.upv.es>
2024-10-28 02:13:54 +0100

    Created a new shared memory for the emulated state machine

--- Inc/HALALMock/Services/SharedMemory/SharedMemory.hpp
+++ Inc/HALALMock/Services/SharedMemory/SharedMemory.hpp
@@ -10,8 +10,15 @@ class SharedMemory {
 
     // do not use before SharedMemory::start, uninitialized!!!
     static EmulatedPin *gpio_memory;
+	static void* emulated_state_machine;
     static EmulatedPin &get_pin(Pin pin);
     static void start();
+	static void start_emulated_state_machine();
+	constexpr static size_t emulated_state_machine_size=16;
+	constexpr char* emulated_state_machine_name="/Shared_Memory_Emulated_State_Machine";
+	static uint8_t* state_machine_sm;
+	static void update_state_machine_counter(uint8_t counter);
+	static void update_current_state(uint8_t index, uint8_t state);
 };
 unordered_map<Pin, size_t> pin_offsets = {
 	{PA0, 0}, {PA1, 1}, {PA2, 2}, {PA3, 3}, {PA4, 4},
--- Src/HALALMock/Services/SharedMemory/SharedMemory.cpp
+++ Src/HALALMock/Services/SharedMemory/SharedMemory.cpp
@@ -1,9 +1,55 @@
 
 #include "HALALMock/Services/SharedMemory/SharedMemory.hpp"
+//includes to create the shared Memory in Posix
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 void SharedMemory::start() {
     // TODO: create / open shared memory
 }
+
+void SharedMemory::start_emulated_state_machine(){
+	// shared memory file descriptor
+	int shm_state_machine_fd;
+
+	// create the shared memory object
+	shm_state_machine_fd=shm_open(emulated_state_machine_name,0_CREAT | 0_RDWR, 0666);
+	if(shm_state_machine_fd==-1){
+		ErrorHandler("%s","Error creating the shared memory object");
+		return;
+	}
+	// configure the size of the shared memory object
+	if(ftruncate(shm_state_machine_fd,emulated_state_machine_size)==-1){
+		ErrorHandler("%s","Error configuring the size of the shared memory object");
+		close(shm_state_machine_fd);
+		return;
+	}
+	// memory map the shared memory object
+	emulated_state_machine=mmap(NULL,emulated_state_machine_size,PROT_WRITE | PROT_READ,MAP_SHARED,shm_state_machine_fd,0);
+	if(emulated_state_machine==MAP_FAILED){
+		ErrorHandler("%s","Error mapping the shared memory object");
+		close(shm_state_machine_fd);
+		return;
+	}
+
+	// cast of the void pointer that the shared memory object returns to a uint8_t pointer
+	state_machine_sm=static_cast<uint8_t*>(emulated_state_machine);
+	// initialize the shared memory counter by setting it to 0, when a new state machine is created it will increment
+	state_machine_sm[0]=0;
+	
+}
+
+void SharedMemory::update_state_machine_counter(uint8_t counter){
+	state_machine_sm[0]=counter;
+}
+
+void SharedMemory::update_current_state(uint8_t index, uint8_t state){
+	state_machine_sm[index]=state;
+}
+
 EmulatedPin &SharedMemory::get_pin(Pin pin){
     uint8_t offset;
 	auto it = pin_offsets.find(pin);
