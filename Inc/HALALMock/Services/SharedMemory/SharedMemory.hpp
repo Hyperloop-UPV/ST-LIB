b693be20c7ebb6fe566e13271b09162c4a2520f2
oganigl <osganig@gmail.com>
2024-11-13 22:33:47 +0100

    light changes, make some functions private

--- Inc/HALALMock/Services/SharedMemory/SharedMemory.hpp
+++ Inc/HALALMock/Services/SharedMemory/SharedMemory.hpp
@@ -13,8 +13,8 @@ class SharedMemory {
 	
   	static EmulatedPin *gpio_memory;
 	static uint8_t* state_machine_memory;
-	char* gpio_memory_name;
-  	constexpr static uint8_t total_pins= 114;
+	static char* gpio_memory_name;
+  	static constexpr  uint8_t total_pins= 114;
 	static constexpr int gpio_memory_size = total_pins * sizeof(EmulatedPin);
   
 	constexpr static size_t state_machine_memory_size=16;
@@ -22,13 +22,16 @@ class SharedMemory {
 
 	static void start();
 	static void start(char* name);
-	static void start_gpio_shared_memory();
-	static void start_state_machine_memory();
-
+	static void close();
+	static void close_gpio_shared_memory();
+	
 	static EmulatedPin &get_pin(Pin pin);
 
 	static void update_current_state(uint8_t index, uint8_t state);
   
 	static unordered_map<Pin, size_t> pin_offsets;
-	
+	private:
+		//Private to avoid bad use of this functions from the user
+		static void start_gpio_shared_memory();
+		static void start_state_machine_memory();
 };
