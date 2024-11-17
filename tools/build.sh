22737f7e0d762b06fa2ca16f58e6f22580187c5c
Gonzalo Sanchez <gonzalosmoya24@gmail.com>
2024-11-17 15:35:00 +0100

    add and fix scripts

--- tools/build.sh
+++ tools/build.sh
@@ -1,13 +1,17 @@
 ## shell script for CI automation
-
+#!/bin/sh
+if [[ $# -eq 0 ]] ; then
+    echo "ERROR, provide a build configuration"
+    exit 1
+fi
 mode=$1
-
+echo $mode
 out_dir="out/build/$mode"
 
-if $mode=="simulator"
-cmake -DCMAKE_BUILD_TYPE=Debug -S . -B $out_dir -G "Unix Makefiles"
-cd $out_dir 
-(make -j 8)
+if [[ "$mode"="simulator" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -S . -B $out_dir -G "Unix Makefiles"
+    cd $out_dir 
+    make -j 8
 else
     echo "hardware build"
 fi # mode=simulator
