e821eb7a3c4621843f2c8c2e410cbb6652c1bd5c
jdmarmen <jdmarmen@etsinf.upv.es>
2024-11-28 03:00:42 +0100

    Added ADC memory tests + small fix in adc.cpp

--- Tests/ADCTest.cpp
+++ Tests/ADCTest.cpp
@@ -3,6 +3,21 @@
 #include "HALALMock/Services/SharedMemory/SharedMemory.hpp"
 #include <iostream>
 
+TEST(ADC, Check_Memory_Writing){
+
+    SharedMemory::start("GPIO_Name", "State_Machine_Name");
+
+    for (int i = 0; i < SharedMemory::total_pins; i++) {
+        ((uint8_t *)(SharedMemory::gpio_memory + i))[1]=1;
+        EXPECT_EQ(((uint8_t *)(SharedMemory::gpio_memory + i))[1],1);
+        ((uint8_t *)(SharedMemory::gpio_memory + i))[1]=0;
+        EXPECT_EQ(((uint8_t *)(SharedMemory::gpio_memory + i))[1],0);
+    }
+
+    SharedMemory::close();
+
+}
+
 TEST(ADC,Inscribe){
 
     SharedMemory::start("GPIO_Name", "State_Machine_Name");
@@ -10,34 +25,23 @@ TEST(ADC,Inscribe){
     //existing pin
     uint8_t adc_id_1=ADC::inscribe(PA0);
     EXPECT_EQ(adc_id_1, 0);
+    uint8_t* PA0_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PA0]);
+    EXPECT_EQ(PA0_memory[0], static_cast<uint8_t>(PinType::ADC));
+
     //not existing pin
     uint8_t adc_id_2=ADC::inscribe(PB15);
     EXPECT_EQ(adc_id_2, 0);
+    uint8_t* PB15_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PB15]);
+    EXPECT_EQ(PB15_memory[0], static_cast<uint8_t>(PinType::NOT_USED));
+
     //existing pin
     uint8_t adc_id_3=ADC::inscribe(PB1);
     EXPECT_EQ(adc_id_3, 1);
+    uint8_t* PB1_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PB1]);
+    EXPECT_EQ(PB1_memory[0], static_cast<uint8_t>(PinType::ADC));
 
     SharedMemory::close();
 }
-
-TEST(ADC,Start){
-    
-        SharedMemory::start("GPIO_Name", "State_Machine_Name");
-    
-        uint8_t adc_id_1=ADC::inscribe(PA0);
-        uint8_t adc_id_2=ADC::inscribe(PB15);
-    
-        ADC::start();
-    
-        EmulatedPin& my_emulated_pin_1=SharedMemory::get_pin(PA0);
-        EmulatedPin& my_emulated_pin_2=SharedMemory::get_pin(PB15);
-    
-        EXPECT_EQ(my_emulated_pin_1.type, PinType::ADC);
-        EXPECT_EQ(my_emulated_pin_2.type, PinType::NOT_USED);
-    
-        SharedMemory::close();
-}
-
 
 TEST(ADC,Get_value){
 
@@ -58,7 +62,7 @@ TEST(ADC,Get_value){
     SharedMemory::close();
 }
 
-TEST(ADC,Get_int_value){
+TEST(ADC,Get_value_Memory){
 
     SharedMemory::start("GPIO_Name", "State_Machine_Name");
 
@@ -67,8 +71,62 @@ TEST(ADC,Get_int_value){
     ADC::start();
     ADC::turn_on(adc_id);
 
+    
+
+    uint8_t offset=SHM::pin_offsets[PA0];
+    uint8_t* pin_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + offset);
+
+    // check if turn_on() is setting the bool_is_on to true
+    bool* bool_is_on=reinterpret_cast<bool*>(pin_memory+3);
+    EXPECT_EQ(*bool_is_on,true);
+
+    uint16_t* adc_value=reinterpret_cast<uint16_t*>(pin_memory+1);
+    *adc_value=32768;
+
+    float value=ADC::get_value(adc_id);
+
+    EXPECT_NEAR(value, 1.65f, 0.1f);
+
+    SharedMemory::close();
+}
+
+TEST(ADC,Get_int_value){
+
+    SharedMemory::start("GPIO_Name", "State_Machine_Name");
+
+    uint8_t adc_id=ADC::inscribe(PB1);
+
+    ADC::start();
+    ADC::turn_on(adc_id);
+
     uint16_t* my_raw_value=ADC::get_value_pointer(adc_id);
     *my_raw_value=32768;
+
+    uint16_t raw_value=ADC::get_int_value(adc_id);
+
+    EXPECT_EQ(raw_value, 32768);
+
+    SharedMemory::close();
+}
+
+TEST(ADC,Get_int_value_Memory){
+
+    SharedMemory::start("GPIO_Name", "State_Machine_Name");
+
+    uint8_t adc_id=ADC::inscribe(PB1);
+
+    ADC::start();
+    ADC::turn_on(adc_id);
+
+    uint8_t offset=SHM::pin_offsets[PB1];
+    uint8_t* pin_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + offset);
+
+    // check if turn_on() is setting the bool_is_on to true
+    bool* bool_is_on=reinterpret_cast<bool*>(pin_memory+3);
+    EXPECT_EQ(*bool_is_on,true);
+
+    uint16_t* adc_value=reinterpret_cast<uint16_t*>(pin_memory+1);
+    *adc_value=32768;
 
     uint16_t raw_value=ADC::get_int_value(adc_id);
 
