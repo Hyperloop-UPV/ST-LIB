309bc0df3d304c7bd146cd084c7c93a2ec4fb696
Ikubimu <ikerubidemunoz@gmail.com>
2024-12-01 10:48:01 +0100

    EXTI test

--- Tests/EXTITest.cpp
+++ Tests/EXTITest.cpp
@@ -15,18 +15,18 @@ void callback_target_2()
     enter_on_callback_2 = true;
 }
 
-void test_interrupts_1(EmulatedPin &pin1, EmulatedPin &pin2)
+void test_interrupts_1(uint8_t* pin1, uint8_t* pin2)
 {
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
     std::unique_lock<std::mutex> lock(ExternalInterrupt::mutex);
-    pin1.PinData.exti.trigger_signal = true;
+    *reinterpret_cast<bool*>(pin1+6) = PinState::ON;
     lock.unlock();
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
     lock.lock();
-    pin2.PinData.exti.trigger_signal = true;
+    *reinterpret_cast<bool*>(pin2+6) = PinState::ON;
     lock.unlock();
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
     
@@ -44,18 +44,25 @@ static uint8_t id1, id2 = 0;
 TEST(EXTITest, Inscribe){
 
     SharedMemory::start("GPIO_Name", "State_Machine_Name");
+    uint8_t* PE0_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE0]);
+    uint8_t* PE7_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE7]);
+    uint8_t* PE1_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE1]);
+
     //try inscribe correct PIN
     id1 = ExternalInterrupt::inscribe(PE0, callback_target_1, TRIGGER::BOTH_EDGES);
+    EXPECT_EQ(PE0_memory[0], static_cast<uint8_t>(PinType::EXTIPin));
     EXPECT_EQ(id1, 1);
     EXPECT_EQ(ExternalInterrupt::id_counter, 1);
 
     //try inscribe incorrect PIN
     uint8_t id = ExternalInterrupt::inscribe(PE7, callback_target_2, TRIGGER::BOTH_EDGES);
+    EXPECT_EQ(PE7_memory[0], static_cast<uint8_t>(PinType::NOT_USED));
     EXPECT_EQ(id, 0);
     EXPECT_EQ(ExternalInterrupt::id_counter, 1);
 
     //try inscribe another PIN
     id2 = ExternalInterrupt::inscribe(PE1, callback_target_2, TRIGGER::BOTH_EDGES);
+    EXPECT_EQ(PE1_memory[0], static_cast<uint8_t>(PinType::EXTIPin));
     EXPECT_EQ(id2, 2);
     EXPECT_EQ(ExternalInterrupt::id_counter, 2);
 
@@ -84,15 +91,15 @@ TEST(EXTITest, TurnOn){
 
     SharedMemory::start("GPIO_Name", "State_Machine_Name");
 
-    EmulatedPin &pin = SharedMemory::get_pin(PE0);
+    uint8_t* pin_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE0]);
 
-    pin.type = PinType::EXTIPin; //Just for testing, SharedMemory is reinit in each test
+    *reinterpret_cast<PinType*>(pin_memory) = PinType::EXTIPin; //Just for testing, SharedMemory is reinit in each test
 
-    ASSERT_FALSE(pin.PinData.exti.is_on);
+    ASSERT_FALSE(*reinterpret_cast<bool*>(pin_memory+5));
 
     ExternalInterrupt::turn_on(id1);
 
-    ASSERT_TRUE(pin.PinData.exti.is_on);
+    ASSERT_TRUE(*reinterpret_cast<bool*>(pin_memory+5));
 
 
     SharedMemory::close();
@@ -103,13 +110,14 @@ TEST(EXTITest, get_pin_value){
 
     SharedMemory::start("GPIO_Name", "State_Machine_Name");
 
-    EmulatedPin &pin = SharedMemory::get_pin(PE1);
+    uint8_t* pin_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE1]);
 
-    pin.type = PinType::EXTIPin; //Just for testing, SharedMemory is reinit in each test
+    *reinterpret_cast<PinType*>(pin_memory) = PinType::EXTIPin; //Just for testing, SharedMemory is reinit in each test
+    *reinterpret_cast<bool*>(pin_memory+6) = PinState::OFF;
 
     bool signal = ExternalInterrupt::get_pin_value(id2);
     ASSERT_FALSE(signal);
-    pin.PinData.exti.trigger_signal = PinState::ON;
+    *reinterpret_cast<bool*>(pin_memory+6) = PinState::ON;
 
     signal = ExternalInterrupt::get_pin_value(id2);
     ASSERT_TRUE(signal);
@@ -122,11 +130,11 @@ TEST(EXTITest, handle_interrupt_1){
     SharedMemory::start("GPIO_Name", "State_Machine_Name");
     ExternalInterrupt::turn_on(id1);
     ExternalInterrupt::turn_on(id2);
-    EmulatedPin &pin1 = SharedMemory::get_pin(PE0);
-    pin1.PinData.exti.trigger_signal = PinState::ON;
+    uint8_t* PE0_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE0]);
+    *reinterpret_cast<bool*>(PE0_memory+6) = PinState::ON;
 
-    EmulatedPin &pin2 = SharedMemory::get_pin(PE1);
-    pin2.PinData.exti.trigger_signal = PinState::ON;
+    uint8_t* PE1_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE1]);
+    *reinterpret_cast<bool*>(PE1_memory+6) = PinState::ON;
 
     enter_on_callback_1 = false;
     enter_on_callback_2 = false;
@@ -148,11 +156,11 @@ TEST(EXTITest, handle_interrupt_1){
 TEST(EXTITest, handle_interrupt_concurrece){
 
     SharedMemory::start("GPIO_Name", "State_Machine_Name");
-    EmulatedPin &pin1 = SharedMemory::get_pin(PE0);
-    pin1.PinData.exti.trigger_signal = PinState::OFF;
+     uint8_t* PE0_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE0]);
+    *reinterpret_cast<bool*>(PE0_memory+6) = PinState::OFF;
 
-    EmulatedPin &pin2 = SharedMemory::get_pin(PE1);
-    pin2.PinData.exti.trigger_signal = PinState::OFF;
+    uint8_t* PE1_memory = reinterpret_cast<uint8_t*>(SharedMemory::gpio_memory + SHM::pin_offsets[PE1]);
+    *reinterpret_cast<bool*>(PE1_memory+6) = PinState::OFF;
 
     enter_on_callback_1 = false;
     enter_on_callback_2 = false;
@@ -163,7 +171,7 @@ TEST(EXTITest, handle_interrupt_concurrece){
     
     //std::thread sim_interrupts = std::thread([&](){test_interrupts_1(pin1, pin2);});
 
-    std::thread sim_interrupts(test_interrupts_1, std::ref(pin1), std::ref(pin2));
+    std::thread sim_interrupts(test_interrupts_1, std::ref(PE1_memory), std::ref(PE0_memory));
     sim_interrupts.join();
     ExternalInterrupt::stop();
     //check if modify global variables
