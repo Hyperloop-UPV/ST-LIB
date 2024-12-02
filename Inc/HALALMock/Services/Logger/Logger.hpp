31be54348504b1cce21ca5d06ffd007a6f652d15
Jorge Sáez <jorgeesg82@gmail.com>
2024-12-02 17:07:38 +0000

    Added FileManager class, bitmask config, and finished implementation

--- Inc/HALALMock/Services/Logger/Logger.hpp
+++ Inc/HALALMock/Services/Logger/Logger.hpp
@@ -1,30 +1,82 @@
 #include <string>
 
+// Overload bit operators for LogConf enum
+constexpr Logger::LogConf operator|(const Logger::LogConf& conf1, const Logger::LogConf& conf2) {
+    return static_cast<Logger::LogConf>(static_cast<int>(conf1) | static_cast<int>(conf2));
+};
+constexpr Logger::LogConf operator&(const Logger::LogConf& conf, const Logger::LogLevel& level) {
+    return static_cast<Logger::LogConf>(static_cast<int>(conf) & static_cast<int>(level));
+};
+constexpr Logger::LogConf operator&(const Logger::LogConf& conf1, const Logger::LogConf& conf2) {
+    return static_cast<Logger::LogConf>(static_cast<int>(conf1) & static_cast<int>(conf2));
+};
+
+/// @brief Check if a flag is activated in a mask
+/// @param mask Mask where you want to check the flag
+/// @param flag Flag which you want to check in the mask
+/// @return If the flag is activated in the mask
+constexpr bool hasFlag(Logger::LogConf mask, Logger::LogConf flag) {
+    return (mask & flag) == flag;
+};
+constexpr bool hasFlag(Logger::LogConf mask, Logger::LogLevel flag) {
+    return (mask & flag) == static_cast<Logger::LogConf>(flag);
+};
+
 class Logger {
   public:
-    enum class LogLevel {
-        DEBUG,
-        INFO,
-        WARNING,
-        ERROR,
-        FATAL
-    };
-    enum class LogDest {
-        Console = 0b10,
-        File = 0b01
+    enum class LogLevel : unsigned int {
+        DEBUG   = 0b00001,
+        INFO    = 0b00010,
+        WARNING = 0b00100,
+        ERROR   = 0b01000,
+        FATAL   = 0b10000,
     };
 
-    static void log(std::string, LogLevel level);
-    static void debug(std::string msg);
-    static void info(std::string msg);
-    static void warning(std::string msg);
-    static void error(std::string msg);
-    static void fatal(std::string msg);
+    /// @brief Bitmask that represents log configuration
+    enum class LogConf : unsigned int {
+        /// @brief Includes Debug messages in logs
+        Debug           = 0b00000001,
+        /// @brief Includes Info messages in logs
+        Info            = 0b00000010,
+        /// @brief Includes Warning messages in logs
+        Warning         = 0b00000100,
+        /// @brief Includes Error messages in logs
+        Error           = 0b00001000,
+        /// @brief Includes Fatal messages in logs
+        Fatal           = 0b00010000,
+        /// @brief Writes logs into the console
+        Console         = 0b00100000,
+        /// @brief Writes logs into a file, defined in filename_log external variable
+        File            = 0b01000000,
+        /// @brief Erase previous logs
+        DestructiveLog  = 0b10000000
+    };
 
-    static void setDest(LogDest dest);
+    static void log(const std::string& msg, const LogLevel level);
+    static void debug(const std::string& msg);
+    static void info(const std::string& msg);
+    static void warning(const std::string& msg);
+    static void error(const std::string& msg);
+    static void fatal(const std::string& msg);
+
+    static void setConf(const LogConf conf);
+    static void setFilename(const std::string& filename);
+
+    friend class FileManager;
 
   private:
-    static int config;
-    static void logToConsole(std::string msg);
-    static void logToFile(std::string msg);
+    static LogConf config;
+    static std::unique_ptr<FileManager> file_manager;
+    static std::string log_filename;
+};
+
+class FileManager {
+  public:
+    FileManager(const std::ios_base::openmode file_mode);
+    void add(const std::string& msg);
+    ~FileManager();
+
+  private:
+    std::ofstream file_stream;
+
 };
--- Src/HALALMock/Services/Logger/Logger.cpp
+++ Src/HALALMock/Services/Logger/Logger.cpp
@@ -1,12 +1,25 @@
 #include "HALALMock/Services/Logger/Logger.hpp"
-#include <ctime>
-#include <iostream>
+#include <ctime> // To manage time
+#include <iostream> // To write in console
+#include <fstream> // To write in a file
+#include <memory> // To manage FileManager ptr
 
-void Logger::log(std::string msg, LogLevel level) {
-    // Format message to include timestamp and Level
-    // Reach timestamp
-    time_t current_time = time(nullptr);
-    std::string timestamp = ctime(&current_time);
+// === Class Logger ===
+
+Logger::LogConf Logger::config = Logger::LogConf::Error
+                                | Logger::LogConf::Fatal
+                                | Logger::LogConf::Console
+                                | Logger::LogConf::File;
+
+std::unique_ptr<FileManager> Logger::file_manager = nullptr;
+
+/// @brief Default log filename. To change it, use setFilename()
+std::string Logger::log_filename = "STLIB.log";
+
+void Logger::log(const std::string& msg, const LogLevel level) {
+
+    // Check if this level is activated in config bitmask
+    if(!hasFlag(config, level)) return;
 
     // Reach Log Level
     std::string log_level;
@@ -23,27 +36,75 @@ void Logger::log(std::string msg, LogLevel level) {
             log_level = "FATAL";
     }
 
+    // Format message to include timestamp and Level
+    // Reach timestamp
+    time_t current_time = time(nullptr);
+    std::string timestamp = ctime(&current_time);
+
     // Reach message to be printed
-    std::string formatted_message = "[" + timestamp + "] " + "[" + log_level + "] " + msg + "\n";
+    std::string formatted_message = "[" + timestamp + "] " + "[" + log_level + "] " + msg;
 
     // Print message where has been configured to print
-    if (config & static_cast<int>(LogDest::Console)) {  // Print msg into console
+    if (hasFlag(config, LogConf::Console)) {  // Print msg into console
         std::cout << formatted_message;
+    }
+
+    if (hasFlag(config, LogConf::File)) {  // Write message into a file
+        file_manager->add(formatted_message);
     }
 }
 
-void Logger::debug(std::string msg) { log(msg, LogLevel::DEBUG); }
+void Logger::debug(const std::string& msg) {
+    log(msg, LogLevel::DEBUG);
+}
 
-void Logger::info(std::string msg) { log(msg, LogLevel::INFO); }
+void Logger::info(const std::string& msg) {
+    log(msg, LogLevel::INFO);
+}
 
-void Logger::warning(std::string msg) { log(msg, LogLevel::WARNING); }
+void Logger::warning(const std::string& msg) {
+    log(msg, LogLevel::WARNING);
+}
 
-void Logger::error(std::string msg) { log(msg, LogLevel::ERROR); }
+void Logger::error(const std::string& msg) {
+    log(msg, LogLevel::ERROR);
+}
 
-void Logger::fatal(std::string msg) { log(msg, LogLevel::FATAL); }
+void Logger::fatal(const std::string& msg) {
+    log(msg, LogLevel::FATAL);
+}
 
-void Logger::setDest(LogDest dest) { config = config | static_cast<int>(dest); }
+/// @brief Sets the Logger configuration
+/// @param conf Configuration of the Logger. It must be a bit mask of LogConf values, using '|' operator.
+void Logger::setConf(const LogConf conf) {
+    config = conf;
+    if (hasFlag(conf, LogConf::File)) {
+        if (hasFlag(conf, LogConf::DestructiveLog)) {
+            Logger::file_manager = std::make_unique<FileManager>(std::ios_base::trunc);
+        } else {
+            Logger::file_manager = std::make_unique<FileManager>(std::ios_base::app);
+        }
+    } 
+}
 
-void Logger::logToConsole(std::string msg) {}
+void Logger::setFilename(const std::string& filename) {
+        log_filename = filename;
+    }
 
-void Logger::logToFile(std::string msg) {}
+// === Class FileManager ===
+
+FileManager::FileManager(const std::ios_base::openmode file_mode) {
+    file_stream = std::ofstream(Logger::log_filename, file_mode);
+    if (!file_stream.is_open()) {
+        std::cerr << "LOGGER ERROR. Couldn't open file " << Logger::log_filename << ", writing logs in STLIB.log" << std::endl;
+        file_stream = std::ofstream("STLIB.log", file_mode);
+    }
+}
+
+void FileManager::add(const std::string& msg) {
+    file_stream << msg << std::endl;
+}
+
+FileManager::~FileManager() {
+    file_stream.close();
+}
