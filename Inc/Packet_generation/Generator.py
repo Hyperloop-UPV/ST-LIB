507f30f33ecd49fede7f822bdfc6c99e08a9264f
Catonplas <dcancat@etsinf.upv.es>
2024-12-24 19:53:52 +0100

    Finishes description data classes for python, now goes the generation

--- Inc/Packet_generation/Generator.py
+++ Inc/Packet_generation/Generator.py
@@ -1,37 +1,32 @@
 import json 
-def MeasurementSearch(measurements:dict, variable:str):
-    for measurment in measurements["measurements"]:
-        if measurment["id"] == variable:
-            return measurment
-    return None
-
-def MeasurementFileSearch(packet:str,measurements:dict):
-    packet_name = packet.split('_')[0]
-    for measurement in measurements:
-        measurement_name = measurement.split('_')[0]
-        if packet_name[0] == measurement_name[0]:
-            return measurement
-    else:
-        return measurements[0]
-class BoardDescription:
+import re
+class BoardDescription:    
     def __init__(self,name:str,board:dict):
         self.name = name
         self.id = board["board_id"]
         self.ip = board["board_ip"]
         i = 0
-        self.packets = []
+        self.packets = {}
         for packets in board["packets"]:
-            measurement = MeasurementFileSearch(packets,board["measurements"])
+            packets_name = re.split(r'_|\.', packets)[0]  
+            self.packets[packets_name] = []
+            measurement = self._MeasurementFileSearch(packets,board["measurements"])
             with open("Inc/Packet_generation/JSON_ADE/boards/" + name+"/" + board["packets"][i]) as f:
                 p= json.load(f)
             with open("Inc/Packet_generation/JSON_ADE/boards/" + name + "/" + measurement) as f:
                 m = json.load(f)
             i += 1
             for packet in p["packets"]:
-                print(packet)
-                print("\n")
-                self.packets.append(PacketDescription(packet,m))
-
+                self.packets[packets_name].append(PacketDescription(packet,m))
+                
+    def _MeasurementFileSearch(self,packet:str,measurements:dict):
+        packet_name = packet.split('_')[0]
+        for measurement in measurements:
+            measurement_name = measurement.split('_')[0]
+            if packet_name[0] == measurement_name[0]:
+                return measurement
+        else:
+            return measurements[0]
 class PacketDescription:
     def __init__(self, packet:dict,measurements:dict):
         self.id =packet["id"]
@@ -47,37 +42,50 @@ class PacketDescription:
 class MeasurmentsDescription:
     def __init__(self,measurements:dict, variable:str):
         self.id = variable
-        measurement = MeasurementSearch(measurements,variable)
+        measurement = self._MeasurementSearch(measurements,variable)
         if measurement is None:
             raise Exception("Measurement not found")
         else:
             self.name = measurement["name"]
             self.type = measurement["type"]
+    
+    def _MeasurementSearch(self,measurements:dict, variable:str):
+        for measurment in measurements["measurements"]:
+            if measurment["id"] == variable:
+                return measurment
+        return None
         
 
 
 with open("Inc/Packet_generation/JSON_ADE/boards.json") as f:
     boards = json.load(f)
 
-with open("Inc/Packet_generation/JSON_ADE/boards/LCU/LCU.json") as f:
-    LCU1 = json.load(f)
-LCU = BoardDescription("LCU",LCU1)
+#with open("Inc/Packet_generation/JSON_ADE/boards/LCU/LCU.json") as f:
+    #LCU1 = json.load(f)
+#LCU = BoardDescription("LCU",LCU1)
 
-print(LCU.name)
-print(LCU.packets[0].name)
-i=0
-for variable in LCU.packets[0].variables:
-    print(variable)
+#print(LCU.name)
+#print(LCU.packets[0].name)
+#i=0
+#for variable in LCU.packets[0].variables:
+    #print(variable)
+    #print("\n")
+    #print(LCU.packets[0].measurements[i].type)
+    #i+=1
+
+for board in boards["boards"]:
+    with open("Inc/Packet_generation/JSON_ADE/" + (boards["boards"][board])) as f:
+        b = json.load(f)
+    board_instance = BoardDescription(board, b)
+    globals()[board] = board_instance
+    
+for packet in OBCCU.packets["control"]:
+    print(packet.name)
+    for variable in packet.variables:
+        print(variable)
+    for measurement in packet.measurements:
+        print(measurement.type)
     print("\n")
-    print(LCU.packets[0].measurements[i].type)
-    i+=1
-
-#for board in boards["boards"]:
-    #with open("Inc/Packet_generation/JSON_ADE/" + (boards["boards"][board])) as f:
-        #b = json.load(f)
-    #board_instance = BoardDescription(board, b)
-    #globals()[board] = board_instance
-
 
      
 
