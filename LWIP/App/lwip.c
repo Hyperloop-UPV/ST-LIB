424d8e6e36183453221812edcb17d223801b9902
Jorge Sáez <jorgeesg82@gmail.com>
2025-02-24 22:42:41 +0100

    Adapted to use new cmake

--- LWIP/App/lwip.c
+++ LWIP/App/lwip.c
@@ -26,9 +26,10 @@
 #include "lwip/sio.h"
 #endif /* MDK ARM Compiler */
 #include "ethernetif.h"
+#include "HALAL/Services/Communication/Ethernet/EthernetHelper.hpp"
 
 /* USER CODE BEGIN 0 */
-
+bool ETH_is_cable_connected = false;
 /* USER CODE END 0 */
 /* Private function prototypes -----------------------------------------------*/
 static void ethernet_link_status_updated(struct netif *netif);
