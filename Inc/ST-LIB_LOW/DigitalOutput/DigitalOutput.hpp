2210547a9e06dd3cbe37a89c42a0ca8fde6279db
Juan Martinez <114561048+jmaralo@users.noreply.github.com>
2025-04-25 12:02:55 +0200

    Add pin lock method to digital out (#502)
    * Add pin lock method to digital out
    
    * Fix method not returning bool in hpp
    
    * Actually fix the naming and return value issue
    
    * Rename force_lock_pin_state to lock_pin_state
    
    * Mock was still returning void T.T
    
    * Fix Return in simulator

--- Inc/ST-LIB_LOW/DigitalOutput/DigitalOutput.hpp
+++ Inc/ST-LIB_LOW/DigitalOutput/DigitalOutput.hpp
@@ -10,17 +10,17 @@
 #include "HALAL/HALAL.hpp"
 
 class DigitalOutput {
-public:
-	DigitalOutput() = default;
-	DigitalOutput(Pin& pin);
+   public:
+    DigitalOutput() = default;
+    DigitalOutput(Pin& pin);
 
-	void turn_on();
-	void turn_off();
-	void toggle();
-	void set_pin_state(PinState state);
-private:
-	Pin pin;
-	uint8_t id;
+    void turn_on();
+    void turn_off();
+    void toggle();
+    void set_pin_state(PinState state);
+    bool lock_pin_state(PinState state);
+
+   private:
+    Pin pin;
+    uint8_t id;
 };
-
-
