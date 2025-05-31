9aba2fe47642b032b89ead968d8f683dc2ad80a5
Jorge Sáez <125664643+jorgesg82@users.noreply.github.com>
2025-05-31 16:07:23 +0200

    Fixed bad-ordered params in PWMSensor (#519)
    * fix (PWMSensor): Fixed bad-ordered params in constructor. Also applied formatter
    
    * Fix (PWMSensor): fixed second constructor

--- Inc/ST-LIB_LOW/Sensors/PWMSensor/PWMSensor.hpp
+++ Inc/ST-LIB_LOW/Sensors/PWMSensor/PWMSensor.hpp
@@ -9,43 +9,41 @@
 #pragma once
 #include "ErrorHandler/ErrorHandler.hpp"
 #include "HALAL/HALAL.hpp"
-template<class Type>
+template <class Type>
 class PWMSensor {
-protected:
-	uint8_t id;
-	Type *frequency;
-	Type *duty_cycle;
-	
+   protected:
+    uint8_t id;
+    Type *frequency;
+    Type *duty_cycle;
 
-public:
-	PWMSensor() = default;
-	PWMSensor(Pin &pin, Type &frequency, Type &duty_cycle);
-	PWMSensor(Pin &pin, Type *frequency, Type *duty_cycle);
-	void read();
-	uint8_t get_id();
+   public:
+    PWMSensor() = default;
+    PWMSensor(Pin &pin, Type &frequency, Type &duty_cycle);
+    PWMSensor(Pin &pin, Type *frequency, Type *duty_cycle);
+    void read();
+    uint8_t get_id();
 };
 
-template<class Type>
-PWMSensor<Type>::PWMSensor(Pin &pin, Type &frequency, Type &duty_cycle) :
-	duty_cycle(&duty_cycle), frequency(&frequency)  {
-		id = InputCapture::inscribe(pin);
-		Sensor::inputcapture_id_list.push_back(id);
+template <class Type>
+PWMSensor<Type>::PWMSensor(Pin &pin, Type &frequency, Type &duty_cycle)
+    : frequency(&frequency), duty_cycle(&duty_cycle) {
+    id = InputCapture::inscribe(pin);
+    Sensor::inputcapture_id_list.push_back(id);
 }
 
-template<class Type>
-PWMSensor<Type>::PWMSensor(Pin &pin, Type *frequency, Type *duty_cycle) :
-	duty_cycle(duty_cycle), frequency(frequency) {
-		id = InputCapture::inscribe(pin);
-		Sensor::inputcapture_id_list.push_back(id);
+template <class Type>
+PWMSensor<Type>::PWMSensor(Pin &pin, Type *frequency, Type *duty_cycle)
+    : frequency(frequency), duty_cycle(duty_cycle) {
+    id = InputCapture::inscribe(pin);
+    Sensor::inputcapture_id_list.push_back(id);
 }
 
-template<class Type>
+template <class Type>
 void PWMSensor<Type>::read() {
-	*duty_cycle = InputCapture::read_duty_cycle(id);
-	*frequency = InputCapture::read_frequency(id);
+    *duty_cycle = InputCapture::read_duty_cycle(id);
+    *frequency = InputCapture::read_frequency(id);
 }
-template<class Type>
+template <class Type>
 uint8_t PWMSensor<Type>::get_id() {
-	return id;
+    return id;
 }
-
