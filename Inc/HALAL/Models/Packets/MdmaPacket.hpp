e57cee7dfe5e6edf1c5f4911d67d029000646f77
Boris Mladenov Beslimov <borisbeslimov@gmail.com>
2025-11-06 15:31:57 +0100

    feat(MdmaPacket): Drop support for containers and add mdma instance as parameter

--- Inc/HALAL/Models/Packets/MdmaPacket.hpp
+++ Inc/HALAL/Models/Packets/MdmaPacket.hpp
@@ -10,32 +10,25 @@
 
 #include "HALAL/Models/Packets/Packet.hpp"
 
+class Mdma; // Placeholder, remove later
 
 /** 
  * @brief Packet that uses MDMA to transfer its data 
  * @note Non-atomic write operations may lead to corrupted / half-updated data if variables change during transfer
+ * @note Don't use with containers of variable size unless sizes are fixed
  */
 template<size_t BufferLength, class... Types>
 class MdmaPacket : public StackPacket<BufferLength, Types...> {
 public:
+    static_assert(!has_container<Types...>::value, "MdmaPacket does not support containers");
     using Base = StackPacket<BufferLength, Types...>;
-    
-    MdmaPacket(uint16_t id, Types*... values)
-        : Base(id, values...) {
-        if constexpr (BufferLength == 0) {
-            // Cache variable sizes to detect changes later
-            // (TODO)
-        }
 
-        // Create descriptor linked list in MDMA
+    MdmaPacket(uint16_t id, Types*... values) = delete;
+
+    MdmaPacket(uint16_t id, Mdma* mdma, Types*... values) : Base(id, values...), mdma(mdma) {
+
+        // Inscribe MDMA list and save id or handle or whatever of the list
         // (TODO)
-    }
-
-    /**
-     * Change buffer pointer to build data into
-     */
-    void set_buffer(uint8_t* new_buffer) {
-        this->buffer = new_buffer;
     }
     
     /**
@@ -49,11 +42,6 @@ public:
      * @brief Build the packet data into provided buffer using MDMA
      */
     uint8_t* build(uint8_t* buffer) {
-        if constexpr (BufferLength == 0) {
-            // Check if any variable size has changed and update descriptors
-            // (TODO)
-        }
-
         // Trigger MDMA transfer
         // (TODO)
 
@@ -62,14 +50,16 @@ public:
     }
     
 private:
+    Mdma* mdma;
     // MDMA descriptor management
     // (TODO)
 };
 
 #if __cpp_deduction_guides >= 201606
 template<class... Types>
-MdmaPacket(uint16_t, Types*... values)
-    -> MdmaPacket<(!has_container<Types...>::value)*total_sizeof<Types...>::value, Types...>;
+MdmaPacket(uint16_t, Mdma*, Types*... values)
+    -> MdmaPacket<total_sizeof<Types...>::value, Types...>;
+
 #endif
 
 #endif // MDMA_PACKET_HPP
