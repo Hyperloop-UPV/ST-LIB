38fd08ba59d7744d5f47773733b2a3c6e789b412
Jorge <jorgecaru3@gmail.com>
2025-11-24 20:44:09 +0100

    fuck constexpr, todo en runtime. feat: inscribe funciona, solo quedaria hacer el start

--- Inc/HALAL/Models/DMA/DMA.hpp
+++ Inc/HALAL/Models/DMA/DMA.hpp
@@ -13,24 +13,52 @@
 #include <array>
 
 #define MAX_STREAMS 16
-
+    
 class DMA {
     public:
+        static constexpr bool is_one_of(auto Instance, auto... Bases);
+
+        static constexpr bool is_spi(auto Instance);
+        
+        static constexpr bool is_i2c(auto Instance);
+
+        static constexpr bool is_adc(auto Instance);
+
+        static constexpr bool is_fmac(auto Instance);
+
         template<auto Instance, uint32_t... Streams>
+        requires (
+            (DMA::is_adc(Instance)  && sizeof...(Streams) == 1) ||
+            (DMA::is_fmac(Instance) && sizeof...(Streams) == 3) ||
+            (!DMA::is_adc(Instance) && !DMA::is_fmac(Instance) && sizeof...(Streams) == 2)
+        )
         static void inscribe_stream(auto handle);
 
-        static void start();
+        static void start();        
+    
+    private:
+        using PeriphVariant = std::variant<
+            SPI_HandleTypeDef*,
+            I2C_HandleTypeDef*,
+            ADC_HandleTypeDef*,
+            FMAC_HandleTypeDef*
+        >;
 
-        static bool is_one_of(auto Instance, auto... Bases);
+        template<typename PeriphHandle>
+        using DmaLinkEntry = std::tuple<
+            PeriphHandle*, // (__HANDLE__) 
+            DMA_HandleTypeDef*, // (__DMA_HANDLE__) 
+            DMA_HandleTypeDef* PeriphHandle::*,   // (__PPP_DMA_FIELD__)
+            IRQn_Type   
+        >;
 
-        static bool is_spi(auto Instance);
-        
-        static bool is_i2c(auto Instance);
-
-        static bool is_adc(auto Instance);
-
-        static bool is_fmac(auto Instance);
-        
+        template<typename PeriphHandle>
+        static DmaLinkEntry<PeriphHandle> make_dma_entry(
+            PeriphHandle* periph,
+            DMA_HandleTypeDef* dma,
+            DMA_HandleTypeDef* PeriphHandle::* member,
+            IRQn_Type irqn
+        );
 
         static uint32_t get_Request(auto Instance, uint8_t i);
         
@@ -57,94 +85,135 @@ class DMA {
         static uint32_t get_PeriphBurst(auto Instance, uint8_t i);
         
         static IRQn_Type get_irqn(auto stream);
-    
-    private:
-        static std::array<DMA_HandleTypeDef, MAX_STREAMS> inscribed_streams;
+
+        static bool is_stream_available(uintptr_t stream);
+
+        static bool is_peripherial_available(uintptr_t peripherial);
+        
+        static std::array<DmaLinkEntry<PeriphVariant>, MAX_STREAMS> inscribed_streams;
         static uint8_t inscribed_index;
 
         static std::set<uintptr_t> used_peripherials;
         static std::set<uintptr_t> used_streams;
-
-        static bool is_stream_available(uintptr_t stream);
-        static bool is_peripherial_available(uintptr_t peripherial);
-
 };
 
 inline uint8_t DMA::inscribed_index{0};
-inline std::set<uintptr_t> DMA::used_peripherals{};
+inline std::set<uintptr_t> DMA::used_peripherials{};
 inline std::set<uintptr_t> DMA::used_streams{};
+inline std::array<DMA::DmaLinkEntry<DMA::PeriphVariant>, MAX_STREAMS> DMA::inscribed_streams{};
 
 template<auto Instance, uint32_t... Streams>
+requires (
+    (DMA::is_adc(Instance)  && sizeof...(Streams) == 1) ||
+    (DMA::is_fmac(Instance) && sizeof...(Streams) == 3) ||
+    (!DMA::is_adc(Instance) && !DMA::is_fmac(Instance) && sizeof...(Streams) == 2)
+)
 void DMA::inscribe_stream(auto handle) {
-    std::size_t N = sizeof...(Streams);
-    ErrorHandler(inscribed_index + N <= MAX_STREAMS, "Too many streams inscribed");
-
+    const std::size_t N = sizeof...(Streams);
+    if (inscribed_index + N > MAX_STREAMS){
+        ErrorHandler("Too many streams inscribed");
+    }
+    
     uintptr_t periph_addr = reinterpret_cast<uintptr_t>(Instance);
-    ErrorHandler(is_peripheral_available(periph_addr), 
-                 "Peripheral already in use");
-
+    if (!is_peripherial_available(periph_addr)){
+        ErrorHandler("Peripheral already in use");
+    }
+    
     std::array<DMA_Stream_TypeDef*, N> streams = {(DMA_Stream_TypeDef*)Streams... };
 
     for (uint8_t i = 0; i < N; i++){
         uintptr_t stream_addr = reinterpret_cast<uintptr_t>(streams[i]);
-        ErrorHandler(is_stream_available(stream_addr), 
-                     "DMA stream already in use");
+        if (!is_stream_available(stream_addr)){
+            ErrorHandler("DMA stream already in use");
+        }
     }
-
-    // Verificación de cantidad según periférico
-    if (is_adc(Instance)) ErrorHandler(N == 1, "ADC DMA must have exactly one stream");
-    else if (is_fmac(Instance)) ErrorHandler(N == 3, "FMAC DMA must have exactly three streams");
-    else ErrorHandler(N == 2, "Peripheral DMA must have exactly two streams (RX and TX)");
 
     for (uint8_t i = 0; i < N; i++){
         used_streams.insert(reinterpret_cast<uintptr_t>(streams[i]));
     }
-    used_peripherals.insert(periph_addr);
+    used_peripherials.insert(periph_addr);
 
     for (uint8_t i = 0; i < N; i++){
-        DMA_HandleTypeDef dma_handle;
-        dma_handle.Init.Request             = get_Request(Instance, i);
-        dma_handle.Init.Direction           = get_Direction(Instance, i);
-        dma_handle.Init.PeriphInc           = get_PeriphInc(Instance, i);
-        dma_handle.Init.MemInc              = get_MemInc(Instance, i);
-        dma_handle.Init.PeriphDataAlignment = get_PeriphDataAlignment(Instance, i);
-        dma_handle.Init.MemDataAlignment    = get_MemDataAlignment(Instance, i);
-        dma_handle.Init.Mode                = get_Mode(Instance, i);
-        dma_handle.Init.Priority            = get_Priority(Instance, i);
-        dma_handle.Init.FIFOMode            = get_FIFOMode(Instance, i);
-        dma_handle.Init.FIFOThreshold       = get_FIFOThreshold(Instance, i);
-        dma_handle.Init.MemBurst            = get_MemBurst(Instance, i);
-        dma_handle.Init.PeriphBurst         = get_PeriphBurst(Instance, i);
-        dma_handle.Instance = streams[i];
-        inscribed_streams[inscribed_index++] = dma_handle;
+        DMA_HandleTypeDef* dma = new DMA_HandleTypeDef{};
+        dma->Instance = streams[i];
+        dma->Init.Request             = get_Request(Instance, i);
+        dma->Init.Direction           = get_Direction(Instance, i);
+        dma->Init.PeriphInc           = get_PeriphInc(Instance, i);
+        dma->Init.MemInc              = get_MemInc(Instance, i);
+        dma->Init.PeriphDataAlignment = get_PeriphDataAlignment(Instance, i);
+        dma->Init.MemDataAlignment    = get_MemDataAlignment(Instance, i);
+        dma->Init.Mode                = get_Mode(Instance, i);
+        dma->Init.Priority            = get_Priority(Instance, i);
+        dma->Init.FIFOMode            = get_FIFOMode(Instance, i);
+        dma->Init.FIFOThreshold       = get_FIFOThreshold(Instance, i);
+        dma->Init.MemBurst            = get_MemBurst(Instance, i);
+        dma->Init.PeriphBurst         = get_PeriphBurst(Instance, i);
+        IRQn_Type irq = get_irqn(streams[i]);
+
+        if constexpr (is_spi(Instance)) {
+            auto member = (i == 0) ? &SPI_HandleTypeDef::hdmarx : &SPI_HandleTypeDef::hdmatx;
+            inscribed_streams[inscribed_index] = make_dma_entry(handle, dma, member, irq);
+        }
+        else if constexpr (is_i2c(Instance)) {
+            auto member = (i == 0) ? &I2C_HandleTypeDef::hdmarx : &I2C_HandleTypeDef::hdmatx;
+            inscribed_streams[inscribed_index] = make_dma_entry(handle, dma, member, irq);
+        }
+        else if constexpr (is_adc(Instance)) {
+            auto member = &ADC_HandleTypeDef::DMA_Handle;
+            inscribed_streams[inscribed_index] = make_dma_entry(handle, dma, member, irq);
+        }
+        else if constexpr (is_fmac(Instance)) {
+            DMA_HandleTypeDef* FMAC_HandleTypeDef::* member;
+            if (i == 0) member = &FMAC_HandleTypeDef::hdmaPreload;
+            else if (i == 1) member = &FMAC_HandleTypeDef::hdmaIn;
+            else member = &FMAC_HandleTypeDef::hdmaOut;
+            inscribed_streams[inscribed_index] = make_dma_entry(handle, dma, member, irq);
+        }
+        
+        inscribed_index++;
     }
+}
+
+template<typename PeriphHandle>
+DMA::DmaLinkEntry<PeriphHandle> DMA::make_dma_entry(
+    PeriphHandle* periph,
+    DMA_HandleTypeDef* dma,
+    DMA_HandleTypeDef* PeriphHandle::* member,
+    IRQn_Type irqn
+){
+    return std::tuple{
+        periph,
+        dma,
+        member,
+        irqn
+    };
 }
 
 bool DMA::is_stream_available(uintptr_t stream) {
     return used_streams.find(stream) == used_streams.end();
 }
 
-bool DMA::is_peripheral_available(uintptr_t peripheral) {
-    return used_peripherals.find(peripheral) == used_peripherals.end();
+bool DMA::is_peripherial_available(uintptr_t peripheral) {
+    return used_peripherials.find(peripheral) == used_peripherials.end();
 }
 
-bool DMA::is_one_of(auto Instance, auto... Bases) {
+constexpr bool DMA::is_one_of(auto Instance, auto... Bases) {
     return ((Instance == Bases) || ...);
 }
 
- bool DMA::is_spi(auto Instance) {
+constexpr bool DMA::is_spi(auto Instance) {
     return is_one_of(Instance, SPI1, SPI2, SPI3, SPI4, SPI5);
 }
 
- bool DMA::is_i2c(auto Instance) {
+constexpr bool DMA::is_i2c(auto Instance) {
     return is_one_of(Instance, I2C1, I2C2, I2C3, I2C5);
 }
 
- bool DMA::is_adc(auto Instance) {
+constexpr bool DMA::is_adc(auto Instance) {
     return is_one_of(Instance, ADC1, ADC2, ADC3);
 }
 
- bool DMA::is_fmac(auto Instance) {
+constexpr bool DMA::is_fmac(auto Instance) {
     return Instance == FMAC;
 } 
 
@@ -200,6 +269,8 @@ uint32_t DMA::get_Request(auto Instance, uint8_t i) {
     if (Instance == FMAC && i == 2) return DMA_REQUEST_FMAC_READ;
     return 0;
 }
+
+
 
 uint32_t DMA::get_Direction(auto Instance, uint8_t i) {
     if (is_fmac(Instance) && i == 0){
