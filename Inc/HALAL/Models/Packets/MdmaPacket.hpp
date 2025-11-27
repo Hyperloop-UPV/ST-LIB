4fb9c6750436307a2ff9ed80ab587fd0cc11e340
Boris Mladenov Beslimov <borisbeslimov@gmail.com>
2025-11-27 11:57:13 +0100

    refactor(MdmaPacket): Make MdmaPacket inherit directly from Packet and use promises

--- Inc/HALAL/Models/Packets/MdmaPacket.hpp
+++ Inc/HALAL/Models/Packets/MdmaPacket.hpp
@@ -9,58 +9,95 @@
 #define MDMA_PACKET_HPP
 
 #include "HALAL/Models/Packets/Packet.hpp"
+#include "HALAL/Models/MDMA/MDMA.hpp"
+#include "HALAL/Models/MPUManager/MPUManager.hpp"
+#include "HALAL/Utils/Promise.hpp"
 
-class Mdma; // Placeholder, remove later
 
-/** 
- * @brief Packet that uses MDMA to transfer its data 
- * @note Non-atomic write operations may lead to corrupted / half-updated data if variables change during transfer
- * @note Don't use with containers of variable size unless sizes are fixed
+/**
+ * @brief A Packet class that uses MDMA for building and parsing packets.
+ * @tparam Types The types of the values in the packet.
+ * @note This class requires MDMA and MPUManager to be properly configured.
+ * @note It uses non-cached memory for MDMA operations.
  */
-template<size_t BufferLength, class... Types>
-class MdmaPacket : public StackPacket<BufferLength, Types...> {
-public:
-    static_assert(!has_container<Types...>::value, "MdmaPacket does not support containers");
-    using Base = StackPacket<BufferLength, Types...>;
-
-    MdmaPacket(uint16_t id, Types*... values) = delete;
-
-    MdmaPacket(uint16_t id, Mdma& mdma, Types*... values) : Base(id, values...), mdma(mdma) {
-
-        // Inscribe MDMA list and save id or handle or whatever of the list
-        // (TODO)
-    }
-    
-    /**
-     * @brief Build the packet data into internal buffer
-     */
-    uint8_t* build() override {
-        // Trigger MDMA transfer with the stored MDMA instance, buffer is managed by the MDMA
-        // (TODO)
-
-        // Fallback until MDMA is implemented
-        return Base::build();
-    }
-
-    /**
-     * @brief Change the MDMA instance used for transfers
-     */
-    void set_mdma(Mdma& new_mdma) {
-        mdma = new_mdma;
-    }
-
-    
-private:
-    Mdma& mdma;
-    // MDMA descriptor management
-    // (TODO)
-};
-
-#if __cpp_deduction_guides >= 201606
 template<class... Types>
-MdmaPacket(uint16_t, Mdma*, Types*... values)
-    -> MdmaPacket<total_sizeof<Types...>::value, Types...>;
+class MdmaPacket : public Packet {
+   public:
+    uint16_t id;
+    uint8_t* buffer;
+    size_t& size = Packet::size;
+    std::tuple<uint16_t*, Types*...> value_pointers;
 
-#endif
+    /**
+     * @brief Constructor for MdmaPacket
+     * @param id The packet ID
+     * @param values Pointers to the values to be included in the packet
+     */
+    MdmaPacket(uint16_t id, Types*... values) 
+    : id(id), size((sizeof(Types) + ...) + sizeof(uint16_t)) , value_pointers(&this->id, values...) {
+        packets[id] = this;
+        // Allocate non-cached buffer for MDMA
+        buffer = MPUManager::allocate_non_cached_memory(size);
+    }
+
+    /**
+     * @brief Build the packet and transfer data into non-cached buffer using MDMA
+     * @param destination_address Optional destination address for the built packet (should be non-cached, else you will need to manage cache coherency)
+     * @return Pointer to the built packet data (internal buffer or destination address)
+     */
+    uint8_t* build(uint8_t* destination_address = nullptr) {
+        Promise* promise = Promise::inscribe();
+        // (TODO) Call MDMA to transfer data
+        promise->wait();
+        return destination_address ? destination_address : buffer;
+    }
+
+    /**
+     * @brief Build the packet and transfer data into non-cached buffer using MDMA with a promise
+     * @param promise Promise to be fulfilled upon transfer completion
+     * @param destination_address Optional destination address for the built packet (should be non-cached, else you will need to manage cache coherency)
+     * @return Pointer to the built packet data (internal buffer or destination address)
+     */
+    uint8_t* build(Promise* promise, uint8_t* destination_address = nullptr) {
+        // (TODO) Call MDMA to transfer data
+        return destination_address ? destination_address : buffer;
+    }
+
+    // Just for interface compliance
+    uint8_t* build() override {
+        return build(nullptr);
+    }
+
+    void parse(uint8_t* data = nullptr) override {
+        Promise* promise = Promise::inscribe();
+        // (TODO) Call MDMA to transfer data
+        promise->wait();
+    }
+
+    void parse(Promise* promise, uint8_t* data = nullptr) {
+        // (TODO) Call MDMA to transfer data
+    }
+
+    size_t get_size() override {
+        return size;
+    }
+
+    uint16_t get_id() override {
+        return id;
+    }
+
+    // Just for interface compliance, this is not efficient for MdmaPacket as it is (could be optimized by adding more functionality to MDMA)
+    // Could be optimized by using a map of index to pointer or similar structure created at compile time.
+    void set_pointer(size_t index, void* pointer) override {
+        size_t current_idx = 0;
+
+        std::apply([&](auto&&... args) {
+            ((current_idx++ == index ? 
+                (args = reinterpret_cast<std::remove_reference_t<decltype(args)>>(pointer)) 
+                : nullptr), ...);
+        }, value_pointers);
+    }
+
+};
 
 #endif // MDMA_PACKET_HPP
