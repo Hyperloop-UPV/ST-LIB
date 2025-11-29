c86413d6fdb2a76fcbf3ae04eb31f1f7111ca880
Jorge Sáez <jorgeesg82@gmail.com>
2025-11-29 13:44:39 +0100

    Fixed modules initialization script path

--- tools/init-submodules.sh
+++ tools/init-submodules.sh
@@ -3,7 +3,14 @@ set -euo pipefail
 
 # This script is supposed to be under ${ST-LIB_PATH}/tools/init-submodules.sh
 
-cd "${0%/*}/.."
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+STLIB_DIR="$(cd -- "${SCRIPT_DIR}/.." && pwd)"
+
+cd "$STLIB_DIR"
 git submodule update --init
-cd STM32CubeH7
-git submodule update --init Drivers/STM32H7xx_HAL_Driver Drivers/CMSIS/Device/ST/STM32H7xx Drivers/BSP/Components/lan8742
+cd "$STLIB_DIR/STM32CubeH7"
+git submodule update --init \
+  Drivers/STM32H7xx_HAL_Driver \
+  Drivers/CMSIS/Device/ST/STM32H7xx \
+  Drivers/BSP/Components/lan8742
+
