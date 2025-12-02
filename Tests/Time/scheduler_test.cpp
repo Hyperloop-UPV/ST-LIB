3d8bb6ba494778da0ba968910399b39d72031ff2
Gonzalo Sánchez <gonzalosmoya24@gmail.com>
2025-12-02 22:39:23 +0100

    test for execution count

--- Tests/Time/scheduler_test.cpp
+++ Tests/Time/scheduler_test.cpp
@@ -16,3 +16,14 @@ TEST(SchedulerTests, TaskRegistration) {
     Scheduler::register_task(10,&fake_workload);
     EXPECT_EQ(Scheduler::tasks_[0].callback,fake_workload);
 }
+
+TEST(SchedulerTests, TaskExecution) {
+    Scheduler::register_task(10,&fake_workload);
+    Scheduler::start();
+    constexpr int NUM_TICKS = 1'000'000;
+    for(int i = 0; i < NUM_TICKS; i++){
+        Scheduler::simulate_ticking();
+        Scheduler::update();
+    }
+    EXPECT_EQ(count,100'000);
+}
