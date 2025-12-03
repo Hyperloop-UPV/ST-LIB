440be7620779658919129c81d358b7a17b7db393
Gonzalo Sánchez <gonzalosmoya24@gmail.com>
2025-12-03 15:51:41 +0100

    add compiler specific and CoreMacros

---
+++ Inc/MockedDrivers/compiler_specific.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+
+This file contains implementatios or altername names for
+ARM GCC compiler that don't work in x86_64
+
+*/
+#define __RBIT                            
+#define __CLZ                             __builtin_clz
