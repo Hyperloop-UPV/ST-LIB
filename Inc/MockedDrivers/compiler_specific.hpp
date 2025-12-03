d0d23e52221b5536584896e9aab3e8f23b1cefe5
Gonzalo Sánchez <gonzalosmoya24@gmail.com>
2025-12-03 16:54:20 +0100

    feat: add DSB and ISB intructions in x86_64

--- Inc/MockedDrivers/compiler_specific.hpp
+++ Inc/MockedDrivers/compiler_specific.hpp
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <cstdint>
 /*
 
 This file contains implementatios or altername names for
@@ -8,3 +8,6 @@ ARM GCC compiler that don't work in x86_64
 */
 #define __RBIT                            
 #define __CLZ                             __builtin_clz
+#define __COMPILER_BARRIER()                asm volatile("" ::: "memory")
+#define __DSB()                         __asm__ volatile ("mfence" ::: "memory");
+#define __ISB()                         __asm__ volatile ("lfence" ::: "memory");
