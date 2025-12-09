4620c38e2f0fdaadc8df967ddcd77d254bedc1ff
victhor <victorlopezcortes25@gmail.com>
2025-12-09 12:48:14 +0100

    Fix: remove active_task_count_ decrement in pop_front() https://github.com/HyperloopUPV-H8/ST-LIB/pull/534#discussion_r2601565840

--- Src/HALAL/Services/Time/Scheduler.cpp
+++ Src/HALAL/Services/Time/Scheduler.cpp
@@ -53,7 +53,6 @@ inline uint8_t Scheduler::front_id() {
 }
 inline void Scheduler::pop_front() {
     // O(1) remove of logical index 0
-    Scheduler::active_task_count_--;
     Scheduler::sorted_task_ids_ >>= 4;
 }
 
