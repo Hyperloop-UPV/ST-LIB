9240c01227f9c1c4486a93991e2c0e9e87374e62
victhor <victorlopezcortes25@gmail.com>
2025-12-19 11:27:37 +0100

    fix: remove need to ignore Woverflow warnings

--- Inc/MockedDrivers/common.hpp
+++ Inc/MockedDrivers/common.hpp
@@ -28,18 +28,21 @@
 # undef MODIFY_REG
 #endif
 
+// this is needed because later I will do a #define uint32_t size_t
+typedef uint32_t u32;
+
 #define SET_BIT(REG, BIT)     ((REG) |= (BIT))
 
-#define CLEAR_BIT(REG, BIT)   ((REG) = static_cast<uint32_t>(static_cast<uint32_t>(REG) & ~static_cast<uint32_t>(BIT)))
+#define CLEAR_BIT(REG, BIT)   ((REG) = static_cast<u32>(static_cast<u32>(REG) & ~static_cast<u32>(BIT)))
 
 #define READ_BIT(REG, BIT)    ((REG) & (BIT))
 
 #define CLEAR_REG(REG)        ((REG) = (0x0))
 
-#define WRITE_REG(REG, VAL)   ((REG) = static_cast<uint32_t>(VAL))
+#define WRITE_REG(REG, VAL)   ((REG) = static_cast<u32>(VAL))
 
 #define READ_REG(REG)         ((REG))
 
-#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(uint32_t)(CLEARMASK))) | (SETMASK)))
+#define MODIFY_REG(REG, CLEARMASK, SETMASK)  WRITE_REG((REG), (((READ_REG(REG)) & (~(u32)(CLEARMASK))) | (SETMASK)))
 
 #define POSITION_VAL(VAL)     (__CLZ(__RBIT(VAL)))
--- Inc/MockedDrivers/stm32h7xx_ll_tim_wrapper.h
+++ Inc/MockedDrivers/stm32h7xx_ll_tim_wrapper.h
@@ -9,15 +9,9 @@
 
 #include "MockedDrivers/mocked_ll_tim.hpp"
 
-// -Woverflow does not do much in stm32h7xx_ll_tim.h
-// the only places where it's used in are in WRITE_REG(reg, ~(bit))
-// where they should use CLEAR_BIT(reg, bit)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Woverflow"
 #define uint32_t size_t
 #include "stm32h7xx_ll_tim.h"
 #undef uint32_t
-#pragma GCC diagnostic pop
 
 #include "MockedDrivers/common.hpp"
 
