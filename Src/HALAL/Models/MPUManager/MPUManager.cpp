33bb954f4bd52291f6776ad5e0cb9f79782edf89
Foniks <borisbeslimov@gmail.com>
2025-12-25 20:28:50 +0100

    fix(MPU): Make the MPUManager take it's buffer pointer from a linker symbol, to ensure initialization order doesn't matter

--- Src/HALAL/Models/MPUManager/MPUManager.cpp
+++ Src/HALAL/Models/MPUManager/MPUManager.cpp
@@ -1,4 +1,6 @@
 #include "HALAL/Models/MPUManager/MPUManager.hpp"
 
-void* MPUManager::no_cached_ram_start = nullptr;
+extern "C" uint8_t __mpu_ram_d3_nc_start[];
+
+void* MPUManager::no_cached_ram_start = __mpu_ram_d3_nc_start;
 uint32_t MPUManager::no_cached_ram_occupied_bytes = 0;
