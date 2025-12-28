a2f06398807921c404c2afbac0e248eb5f26d2d1
Foniks <borisbeslimov@gmail.com>
2025-12-28 22:40:28 +0100

    fix(SPI): Callback handling

--- Src/HALAL/Models/SPI2.cpp
+++ Src/HALAL/Models/SPI2.cpp
@@ -68,18 +68,21 @@ void SPI6_IRQHandler(void) {
 
 void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi) {
     ST_LIB::SPIDomain::Instance* inst = nullptr;
-    if (hspi == spi_instances[0]->hspi) {
+    if (spi_instances[0] != nullptr && hspi == &spi_instances[0]->hspi) {
         inst = spi_instances[0];
-    } else if (hspi == spi_instances[1]->hspi) {
+    } else if (spi_instances[1] != nullptr && hspi == &spi_instances[1]->hspi) {
         inst = spi_instances[1];
-    } else if (hspi == spi_instances[2]->hspi) {
+    } else if (spi_instances[2] != nullptr && hspi == &spi_instances[2]->hspi) {
         inst = spi_instances[2];
-    } else if (hspi == spi_instances[3]->hspi) {
+    } else if (spi_instances[3] != nullptr && hspi == &spi_instances[3]->hspi) {
         inst = spi_instances[3];
-    } else if (hspi == spi_instances[4]->hspi) {
+    } else if (spi_instances[4] != nullptr && hspi == &spi_instances[4]->hspi) {
         inst = spi_instances[4];
-    } else if (hspi == spi_instances[5]->hspi) {
+    } else if (spi_instances[5] != nullptr && hspi == &spi_instances[5]->hspi) {
         inst = spi_instances[5];
+    } else {
+        ErrorHandler("SPI IRQ Callback called but instance is null");
+        return;
     }
 
     if (inst->operation_flag != nullptr) {
@@ -98,18 +101,21 @@ void HAL_SPI_TxRxCpltCallback(SPI_HandleTypeDef *hspi) {
 void HAL_SPI_ErrorCallback(SPI_HandleTypeDef *hspi) {
     uint32_t error_code = hspi->ErrorCode;
     uint32_t inst_idx = 0;
-    if (hspi == spi_instances[0]->hspi) {
+    if (spi_instances[0] != nullptr && hspi == &spi_instances[0]->hspi) {
         inst_idx = 0;
-    } else if (hspi == spi_instances[1]->hspi) {
+    } else if (spi_instances[1] != nullptr && hspi == &spi_instances[1]->hspi) {
         inst_idx = 1;
-    } else if (hspi == spi_instances[2]->hspi) {
+    } else if (spi_instances[2] != nullptr && hspi == &spi_instances[2]->hspi) {
         inst_idx = 2;
-    } else if (hspi == spi_instances[3]->hspi) {
+    } else if (spi_instances[3] != nullptr && hspi == &spi_instances[3]->hspi) {
         inst_idx = 3;
-    } else if (hspi == spi_instances[4]->hspi) {
+    } else if (spi_instances[4] != nullptr && hspi == &spi_instances[4]->hspi) {
         inst_idx = 4;
-    } else if (hspi == spi_instances[5]->hspi) {
+    } else if (spi_instances[5] != nullptr && hspi == &spi_instances[5]->hspi) {
         inst_idx = 5;
+    } else {
+        ErrorHandler("SPI IRQ Callback called but instance is null");
+        return;
     }
 
     ErrorHandler("SPI%i failed with error number %u", inst_idx + 1, error_code);
