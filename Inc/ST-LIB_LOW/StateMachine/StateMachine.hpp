7c9b1771101cc275ea2d1f88cdc8666f3b17ccce
Cantonplas <dcancat@etsinf.upv.es>
2026-01-21 18:27:26 +0100

    Moved static_vector to a seperate file

--- Inc/ST-LIB_LOW/StateMachine/StateMachine.hpp
+++ Inc/ST-LIB_LOW/StateMachine/StateMachine.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "C++Utilities/CppUtils.hpp"
+#include "C++Utilities/StaticVector.hpp"
 #include "ErrorHandler/ErrorHandler.hpp"
 #include "HALAL/HALAL.hpp"
 #include <array>
@@ -20,6 +21,9 @@
 using ms = std::chrono::milliseconds;
 using us = std::chrono::microseconds;
 using s = std::chrono::seconds;
+
+template <typename T, size_t Capacity>
+using FixedVector = StaticVector<T, Capacity>;
 
 template<class StateEnum>
 concept IsEnum = std::is_enum_v<StateEnum>;
@@ -46,57 +50,6 @@ public:
   TimedAction() = default;
   constexpr bool operator==(const TimedAction&) const = default;
 };
-
-template <typename T, size_t Capacity>
-class StaticVector {
- private: 
-    std::array<T, Capacity> data{};
-    size_t size_ = 0;
-
-public:
-    constexpr StaticVector() = default;
-    
-    template<typename... Args>
-    constexpr StaticVector(Args&&... args) : data{std::forward<Args>(args)...}, size_(sizeof...(args)) {}
-
-    constexpr bool operator==(const StaticVector&) const = default;
-
-    constexpr void push_back(const T& value) 
-    {
-        if (size_ >= Capacity) 
-        {
-            ErrorHandler("StaticVector capacity exceeded");
-            return;
-        }        
-        data[size_] = value;
-        size_++;
-    }
-
-    constexpr auto begin() { return data.begin(); }
-    constexpr auto begin() const { return data.begin(); }
-    constexpr auto end() { return data.begin() + size_; } 
-    constexpr auto end() const { return data.begin() + size_; } 
-    
-    constexpr const std::array<T, Capacity>& get_array() const { return data; }
-    constexpr size_t size() const { return size_; }
-    constexpr T* get_data() { return data.data(); }
-    constexpr const T* get_data() const { return data.data(); }
-    constexpr  T& operator[](size_t i) { return data[i]; }
-    constexpr  const T& operator[](size_t i) const { return data[i]; }
-    constexpr bool contains(const T& value) const 
-    {
-        for (size_t i = 0; i < size_; ++i) 
-        {
-            if (data[i] == value) 
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-};
-template <typename T, size_t Capacity>
-using FixedVector = StaticVector<T, Capacity>;
 
 template <IsEnum StateEnum>
 struct Transition {
@@ -309,7 +262,6 @@ private:
   };
 
 public:
-  using state_id = StateEnum;
   StateEnum current_state;
 
   constexpr ~StateMachine() override = default;
