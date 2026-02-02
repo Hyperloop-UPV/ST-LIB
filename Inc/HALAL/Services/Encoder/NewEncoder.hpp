8f81ff7830d43708a10f45ac7077430dbeea3e01
victhor <victorlopezcortes25@gmail.com>
2026-02-02 12:06:17 +0100

    Pins must be of different channels

--- Inc/HALAL/Services/Encoder/NewEncoder.hpp
+++ Inc/HALAL/Services/Encoder/NewEncoder.hpp
@@ -15,6 +15,7 @@ struct Encoder {
     static_assert(dev.e.pin_count == 2, "Encoder must have exactly 2 encoder pins, as it uses the whole timer");
     static_assert(dev.e.pins[0].af == TimerAF::Encoder, "Pin 0 must be declared as encoder");
     static_assert(dev.e.pins[1].af == TimerAF::Encoder, "Pin 1 must be declared as encoder");
+    static_assert(dev.e.pins[0].channel != dev.e.pins[1].channel, "Pins must be of different channels");
 
     static TimerWrapper<dev> *timer;
     static void init(TimerWrapper<dev> *timer, uint16_t prescaler, uint32_t period);
