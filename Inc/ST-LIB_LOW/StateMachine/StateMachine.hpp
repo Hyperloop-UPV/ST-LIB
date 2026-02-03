b2939fc96b3cb8bc9c2a862f35db6ddbda6e016b
Cantonplas <dcancat@etsinf.upv.es>
2026-02-03 18:22:37 +0100

    Deleted useless function i added sorry :p

--- Inc/ST-LIB_LOW/StateMachine/StateMachine.hpp
+++ Inc/ST-LIB_LOW/StateMachine/StateMachine.hpp
@@ -512,10 +512,6 @@ public:
       state = current_state;
   }
 
-  void get_current_state(uint8_t& state) const
-  {
-      state = static_cast<uint8_t>(current_state);
-  }
 
 
   inline void refresh_state_orders()
