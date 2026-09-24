5110cb2722e51cee80f91e88e6e7ab442f25c7a7
victhor <victorlopezcortes25@gmail.com>
2026-09-24 12:53:31 +0200

    formatting part 1

--- Inc/HALAL/Services/Communication/SNTP/SNTP.hpp
+++ Inc/HALAL/Services/Communication/SNTP/SNTP.hpp
@@ -21,8 +21,8 @@ struct SNTP {
     static constexpr uint16_t PORT = 8123;
     static constexpr uint8_t MAX_SERVERS = 1;
     static constexpr uint32_t RETRY_TIMEOUT = 15000;
-    static constexpr uint32_t RECV_TIMEOUT  = 15000;
-    static constexpr uint32_t UPDATE_DELAY  = 3600000;
+    static constexpr uint32_t RECV_TIMEOUT = 15000;
+    static constexpr uint32_t UPDATE_DELAY = 3600000;
 
     static constexpr uint8_t OPMODE_POLL = 0;
     static constexpr uint8_t OPMODE_LISTENONLY = 1;
@@ -38,27 +38,27 @@ struct SNTP {
     static constexpr uint16_t MSG_LEN = 48;
 
     /* NOTE: We don't care about leap seconds so we'll use no-warning */
-    static constexpr uint8_t LEAP_INDICATOR_NO_WARNING         = 0x00 << 6;
+    static constexpr uint8_t LEAP_INDICATOR_NO_WARNING = 0x00 << 6;
     static constexpr uint8_t LEAP_INDICATOR_LAST_MINUTE_61_SEC = 0x01 << 6;
     static constexpr uint8_t LEAP_INDICATOR_LAST_MINUTE_59_SEC = 0x02 << 6;
-    static constexpr uint8_t LEAP_INDICATOR_ALARM_CONDITION    = 0x03 << 6;
+    static constexpr uint8_t LEAP_INDICATOR_ALARM_CONDITION = 0x03 << 6;
 
     /* SNTP Version 4 */
     static constexpr uint8_t VERSION = 4 << 3;
 
-    static constexpr uint8_t MODE_MASK      = 0x07;
-    static constexpr uint8_t MODE_CLIENT    = 0x03;
-    static constexpr uint8_t MODE_SERVER    = 0x04;
+    static constexpr uint8_t MODE_MASK = 0x07;
+    static constexpr uint8_t MODE_CLIENT = 0x03;
+    static constexpr uint8_t MODE_SERVER = 0x04;
     static constexpr uint8_t MODE_BROADCAST = 0x05;
 
     static constexpr uint8_t OFFSET_STRATUM = 1;
-    static constexpr uint8_t STRATUM_KOD    = 0x00;
+    static constexpr uint8_t STRATUM_KOD = 0x00;
 
     static constexpr uint8_t OFFSET_TRANSMIT_TIME = 40;
 
     struct Server {
 #if SNTP_SERVER_DNS
-        const char *name;
+        const char* name;
 #endif /* SNTP_SERVER_DNS */
         ip_addr_t addr;
 #if SNTP_MONITOR_SERVER_REACHABILITY
@@ -104,7 +104,7 @@ struct SNTP {
 #if SNTP_SUPPORT_MULTIPLE_SERVERS
     /** The currently used server (initialized to 0) */
     static inline uint8_t sntp_current_server;
-    static inline void try_next_server(const ip_addr_t *server_addr) {
+    static inline void try_next_server(const ip_addr_t* server_addr) {
         #error SNTP multiple servers not supported.
     }
 #else /* SNTP_SUPPORT_MULTIPLE_SERVERS */
@@ -112,11 +112,11 @@ struct SNTP {
 #endif /* SNTP_SUPPORT_MULTIPLE_SERVERS */
 
     /** The UDP pcb used by the SNTP client */
-    static inline struct udp_pcb *pcb;
+    static inline struct udp_pcb* pcb;
     static inline uint32_t retry_timeout = SNTP::RETRY_TIMEOUT;
 
     static void start(ip_addr_t address);
-    static void start(const char *ip);
+    static void start(const char* ip);
     static void start(uint8_t address_head, uint8_t address_second, uint8_t address_third, uint8_t address_last);
     static void stop(void);
 
@@ -126,12 +126,12 @@ struct SNTP {
     static inline uint16_t try_next_server_task_id = Scheduler::INVALID_ID;
     static inline uint8_t opmode = SNTP::MODE;
 
-    static void initialize_request(SNTP::Message *req);
-    static void send_request(const ip_addr_t *server_addr);
+    static void initialize_request(SNTP::Message* req);
+    static void send_request(const ip_addr_t* server_addr);
     static void retry(void);
     static void request(void);
-    static void process(const SNTP::Timestamps *timestamps);
-    static void recv(void *arg, struct udp_pcb *pcb, struct pbuf *p, const ip_addr_t *addr, u16_t port);
+    static void process(const SNTP::Timestamps* timestamps);
+    static void recv(void *arg, struct udp_pcb* pcb, struct pbuf* p, const ip_addr_t* addr, u16_t port);
 
 #if !SNTP_SUPPORT_MULTIPLE_SERVERS
     static constexpr void (*try_next_server)(void) = SNTP::retry;
